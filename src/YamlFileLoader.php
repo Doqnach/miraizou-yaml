04bd2189b5a9d4124e1e7df77e38dc0fe876672d
Doqnach <doqnach@miraizou.net>
2019-02-22 17:27:08 +0100

    initial

---
+++ src/YamlFileLoader.php
@@ -0,0 +1,85 @@
+<?php
+declare(strict_types=1);
+
+namespace Miraizou\Yaml;
+
+use Symfony\Component\Config\Loader\Loader;
+use Symfony\Component\Filesystem\Exception\FileNotFoundException;
+use Symfony\Component\Yaml\Tag\TaggedValue;
+use Symfony\Component\Yaml\Yaml;
+use function JmesPath\search;
+
+class YamlFileLoader extends Loader
+{
+
+    /**
+     * Loads a resource.
+     *
+     * @param mixed $resource The resource
+     * @param string|null $type The resource type or null if unknown
+     * @param YamlParser $parser optional parser instance to use (for e.g. included sub-documents)
+     *
+     * @return mixed
+     *
+     * @throws \Exception If something went wrong
+     */
+    public function load($resource, $type = null, YamlParser $parser = null)
+    {
+        if (!\is_file($resource) || !\is_readable($resource)) {
+            throw new FileNotFoundException(null, 0, null, $resource);
+        }
+
+        $basepath = \dirname(\realpath($resource));
+
+        $parser = $parser ?? new YamlParser();
+        $configValues = $parser->parseFile($resource, Yaml::PARSE_CUSTOM_TAGS|YamlParser::PARSE_KEEP_REFS);
+
+        \array_walk_recursive($configValues, function(&$item/*, $key*/) use ($parser, $basepath) {
+            if ($item instanceof TaggedValue && $item->getTag() === 'inc/file') {
+                $lines = \preg_split("/\n/",$item->getValue(), -1, PREG_SPLIT_NO_EMPTY);
+
+                $items = [];
+                foreach($lines as $line) {
+                    [$file,$pointer] = \explode('#', $line, 2);
+                    if ($file !== '') {
+                        $filepath = $file;
+                        if (\preg_match('`^(?:\w+:/)?/`', $file) !== 1) {
+                            $filepath = $basepath . DIRECTORY_SEPARATOR . $file;
+                        }
+
+                        try {
+                            $innerConfig = $this->load($filepath, null, $parser);
+
+                            if ($pointer === null) {
+                                $items[] = $innerConfig;
+                            } else {
+                                $items[] = search($pointer, $innerConfig);
+                            }
+                        } catch (FileNotFoundException $e) {
+                            throw $e;
+                        }
+                    }
+                }
+                // TODO does this work in all cases?
+                $item = \array_merge(...$items);
+            }
+        });
+
+        return $configValues;
+    }
+
+    /**
+     * Returns whether this class supports the given resource.
+     *
+     * @param mixed $resource A resource
+     * @param string|null $type The resource type or null if unknown
+     *
+     * @return bool True if this class supports the given resource, false otherwise
+     */
+    public function supports($resource, $type = null) : bool {
+        return \is_string($resource) && \in_array(\pathinfo(
+            $resource,
+            PATHINFO_EXTENSION
+        ), ['yaml','yml']);
+    }
+}
---
+++ src/YamlParser.php
@@ -0,0 +1,98 @@
+<?php
+declare(strict_types=1);
+
+namespace Miraizou\Yaml;
+
+use Miraizou\Core\ModifyParentPrivates;
+use ReflectionException;
+use Symfony\Component\Yaml\Exception\ParseException;
+use Symfony\Component\Yaml\Parser;
+
+class YamlParser extends Parser
+{
+    use ModifyParentPrivates;
+
+    public const PARSE_KEEP_REFS = 65536;
+
+    /**
+     * Parses a YAML file into a PHP value.
+     *
+     * @param string $filename The path to the YAML file to be parsed
+     * @param int    $flags    A bit field of PARSE_* constants to customize the YAML parser behavior
+     *
+     * @return mixed The YAML converted to a PHP value
+     *
+     * @throws ParseException If the file could not be read or the YAML is not valid
+     */
+    public function parseFile(string $filename, int $flags = 0)
+    {
+        try {
+            if (!is_file($filename)) {
+                throw new ParseException(sprintf('File "%s" does not exist.', $filename));
+            }
+
+            if (!is_readable($filename)) {
+                throw new ParseException(sprintf('File "%s" cannot be read.', $filename));
+            }
+
+
+            try {
+                return $this->parse(file_get_contents($filename), $flags);
+            } finally {
+                $this->setPrivateProperty('filename', null);
+            }
+        } catch (ReflectionException $e) {
+            return parent::parseFile($filename, $flags);
+        }
+    }
+
+    /**
+     * Parses a YAML string to a PHP value.
+     *
+     * @param string $value A YAML string
+     * @param int    $flags A bit field of PARSE_* constants to customize the YAML parser behavior
+     *
+     * @return mixed A PHP value
+     *
+     * @throws ParseException If the YAML is not valid
+     */
+    public function parse(string $value, int $flags = 0)
+    {
+        try {
+            if (false === preg_match('//u', $value)) {
+                throw new ParseException('The YAML value does not appear to be valid UTF-8.', -1, null, $this->getPrivateProperty('filename'));
+            }
+
+            if (!(self::PARSE_KEEP_REFS & $flags)) {
+                $this->setPrivateProperty('refs', []);
+            }
+
+            $mbEncoding = null;
+            $data = null;
+
+            if (2 /* MB_OVERLOAD_STRING */ & (int) ini_get('mbstring.func_overload')) {
+                $mbEncoding = mb_internal_encoding();
+                mb_internal_encoding('UTF-8');
+            }
+
+            try {
+                $data = $this->invokePrivateMethod('doParse', $value, $flags);
+            } finally {
+                if (null !== $mbEncoding) {
+                    mb_internal_encoding($mbEncoding);
+                }
+                $this->setPrivateProperty('lines', []);
+                $this->setPrivateProperty('currentLine', '');
+                if (!(self::PARSE_KEEP_REFS & $flags)) {
+                    $this->setPrivateProperty('refs', []);
+                }
+                $this->setPrivateProperty('skippedLineNumbers', []);
+                $this->setPrivateProperty('locallySkippedLineNumbers', []);
+            }
+
+            return $data;
+        } catch (ReflectionException $e) {
+            return parent::parse($value, $flags);
+        }
+    }
+}
